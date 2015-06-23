0a9ad305b219505176830181ff3cd4e888899317
aarondh <aaron@daisley-harrison.com>
2015-06-23 14:21:53 -0700

    Create angular-sharepoint.directives.js

---
+++ angular-sharepoint.directives.js
@@ -0,0 +1,10 @@
+/*
+ * angular-sharepoint.directives.js
+ *
+ * Written By: Aaron G. Daisley-Harrison <aaron@daisley-harrison.com>
+ *
+ * angular-sharepoint.directives.js an auxilary file for use with angular-sharepoint
+ * to provide a generic way of including additional directive definitions to the angular-sharepoint module. 
+ *
+ * For more information on angular please visit http://angular.org
+ */
